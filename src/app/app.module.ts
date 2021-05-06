e8168f74d12d298372c03e20f029ce34dd5d261d
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2021-05-06 17:35:10 +0530

    add: nestedComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductSectionComponent } from './product-section/product-section.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductSectionComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
